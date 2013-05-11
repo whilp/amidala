52923877113700f566d74b16274ed67d906a66c9
Will Maier <wcmaier@m.aier.us>
2013-05-11 19:30:33 +0000

    Verbose.

--- amidala/cli.py
+++ amidala/cli.py
@@ -1,10 +1,12 @@
 """amidala
 
 Usage:
-        amidala
+        amidala [options]
+
 Options:
         -h --help       help
         -v --version    version
+        -V --verbose=n  verbosity [default: 0]
 """
 
 import logging
@@ -18,9 +20,14 @@ log = logging.getLogger("amidala.cli")
 
 def main():
     args = docopt.docopt(__doc__, version=amidala.__version__)
+
+    print args
     log.addHandler(logging.StreamHandler())
+    log.level = log_level(int(args["--verbose"]))
 
     if args["--version"]:
         sys.stdout.write(amidala.__version__)
         return 0
 
+def log_level(n, default=logging.ERROR):
+    return max(default - (10 * n), 1)
