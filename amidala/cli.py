c9d3bc18579f47bdd0bffd002e5e7a35c34cd026
Will Maier <wcmaier@m.aier.us>
2013-05-11 19:22:51 +0000

    logging

--- amidala/cli.py
+++ amidala/cli.py
@@ -7,14 +7,19 @@ Options:
         -v --version    version
 """
 
+import logging
 import sys
 
 import docopt
 
 import amidala
 
+log = logging.getLogger("amidala.cli")
+
 def main():
     args = docopt.docopt(__doc__, version=amidala.__version__)
+    log.addHandler(logging.StreamHandler())
+
     if args["--version"]:
         sys.stdout.write(amidala.__version__)
         return 0
