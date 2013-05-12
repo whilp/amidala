087ad32dcb56a55609f260797e21d1c1bfd86324
Will Maier <wcmaier@m.aier.us>
2013-05-12 14:38:40 +0000

    Stab in flow.

--- amidala/cli.py
+++ amidala/cli.py
@@ -1,7 +1,7 @@
 """amidala
 
 Usage:
-        amidala [options]
+        amidala [options] <build>
 
 Options:
         -h --help       help
@@ -10,8 +10,12 @@ Options:
 """
 
 import logging
+import subprocess
 import sys
+import tempfile
 
+import boto.ec2
+import boto.utils
 import docopt
 
 import amidala
@@ -27,6 +31,48 @@ def main():
     if args["--version"]:
         sys.stdout.write(amidala.__version__)
         return 0
+
+    region = "us-west-1"
+    ec2 = boto.ec2.connect_to_region(region)
+
+    instance = boto.utils.get_instance_metadata()["instance_id"]
+    device = "/dev/xvdb"
+
+    volume = None
+    size = 10
+    if volume:
+        parent = ec2.get_all_volumes([volume])
+        snapshot = ec2.create_snapshot(parent)
+        volume = ec2.create_volume(parent.size, parent.zone, snapshot=snapshot)
+    else:    
+        volume = ec2.create_volume(size, region)
+
+    ec2.attach_volume(volume.id, instance, device)
+
+    mount = tempfile.mkdtemp()
+
+    ret = subprocess.call(["sudo", "mount", device, mount])
+
+    exe = args["<build>"]
+    subprocess.call(exe)
+
+    ret = subprocess.call(["sudo", "umount", mount])
+
+    ec2.detach_volume(volume.id, instance, device)
+
+    snap = ec2.create_snapshot(volume)
+
+    ebs = boto.ec2.blockdevicemapping.EBSBlockDeviceType(snapshot_id=snap.id)
+    blocks = boto.ec2.blockdevicemapping.BlockDeviceMapping()
+    blocks["/dev/sda1"] = ebs
+    
+    ami = ec2.register_image(
+        name="amidala-test",
+        architecture="x86_64",
+        #kernel_id
+        #ramdisk_id
+        root_device_name="/dev/sda1",
+        block_device_map=blocks)
 
 def log_level(n, default=logging.ERROR):
     return max(default - (10 * n), 1)
